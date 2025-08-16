a7abd4c6551732e2945f13d77383d8d6d2da66ad
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-08-16 21:12:46 +0700

    feat: add navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import "@fontsource/plus-jakarta-sans";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,15 +9,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="font-plus-jakarta-sans antialiased bg-white text-black min-h-screen"
+        style={{ fontFamily: '"Plus Jakarta Sans Variable", sans-serif' }}
       >
-        {children}
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col min-h-screen">
+          {children}
+        </div>
       </body>
     </html>
   );
