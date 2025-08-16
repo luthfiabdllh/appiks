8a830dc93c5c80494315f2d7448f895f4edd5b72
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-08-16 22:10:08 +0700

    feat: add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Footer7 } from "@/components/components/footer";
 import { Navbar1 } from "@/components/components/navbar";
+import { Hero34 } from "@/components/hero34";
 
 export default function Home() {
   return (
     <>
       <Navbar1 />
+      <Hero34 />
       <Footer7 />
     </>
   );
