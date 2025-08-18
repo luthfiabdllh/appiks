2ecec999ff648e40ce7760f8a3dea08e011f7a1e
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-08-18 21:11:00 +0700

    fix: fix login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,9 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
 import { LoginForm } from "@/components/components/auth/login-form"
-import Link from "next/link"
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link href="/" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          <h2 className="text-4xl font-semibold">Appiks</h2>
-        </Link>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-5xl">
         <LoginForm />
       </div>
     </div>
--- src/components/components/auth/login-form.tsx
+++ src/components/components/auth/login-form.tsx
@@ -1,16 +1,10 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Image from "next/image";
 import Link from "next/link";
-import { FcGoogle } from "react-icons/fc";
 
 export function LoginForm({
   className,
@@ -18,54 +12,59 @@ export function LoginForm({
 }: React.ComponentProps<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-xl">Masuk ke Akun</CardTitle>
-          <CardDescription>
-            Isi data dibawah ini untuk masuk ke akun Anda
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid gap-6">
-              <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <Link
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </Link>
-                  </div>
-                  <Input id="password" type="password" required placeholder="Masukkan Password"/>
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-                <Button type="submit" variant="outline" className="w-full">
-                  <FcGoogle className="mr-2 h-4 w-4 inline" />
-                  Login with Google
-                </Button>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid md:grid-cols-2 p-5">
+          <div className="bg-muted relative hidden md:block rounded-l-lg overflow-hidden">
+            <Image
+              width={300}
+              height={300}
+              src="/image/loginPic.svg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+            <div className="p-4 md:px-10 md:py-14 flex flex-col items-center w-full">
+            <h3 className="text-3xl md:text-5xl text-center font-normal mb-8">Appiks</h3>
+            <form className="border p-4 md:p-6 rounded-lg w-full max-w-xs">
+              <div className="flex flex-col gap-6">
+              <div className="flex flex-col">
+                <h1 className="font-bold text-lg md:text-xl">Masuk ke Akun</h1>
+                <p className="text-muted-foreground text-sm md:text-base">
+                Isi data dibawah ini untuk masuk ke akun Anda
+                </p>
               </div>
-            </div>
-          </form>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="#"
+                  className="ml-auto text-xs md:text-sm underline-offset-2 hover:underline"
+                >
+                  Forgot your password?
+                </Link>
+                </div>
+                <Input id="password" type="password" placeholder="masukkan password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+              </div>
+            </form>
+          </div>
         </CardContent>
       </Card>
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </div>
     </div>
   );
 }
